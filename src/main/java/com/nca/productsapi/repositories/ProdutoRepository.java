ad07b7de7d51accfa6a6ed5f2a5df8b3930ed100
alison <adevpr11@gmail.com>
2026-06-01 10:28:07 -0300

    Adiciona ProdutoRepository.java

---
+++ src/main/java/com/nca/productsapi/repositories/ProdutoRepository.java
@@ -0,0 +1,31 @@
+package com.nca.productsapi.repositories;
+
+import com.nca.productsapi.entities.Produto;
+import com.nca.productsapi.factories.ConnectionFactory;
+
+import java.sql.SQLException;
+
+public class ProdutoRepository {
+
+    public void inserir(Produto obj) throws Exception {
+
+        try (var connection = ConnectionFactory.getConnection()) {
+
+            var statement = connection.prepareStatement(
+                    """
+                            INSERT INTO produtos(nome, descricao, preco, quantidade)
+                              values(?,?,?,?)
+                            """);
+
+            statement.setString(1, obj.getNome());
+            statement.setString(2, obj.getDescricao());
+            statement.setDouble(3, obj.getPreco());
+            statement.setDouble(4, obj.getQuantidade());
+
+            statement.execute();
+
+        } catch (SQLException e) {
+            throw new Exception("ERRO AO INSERIR DADOS: " + e.getMessage());
+        }
+    }
+}
