6d413c1f305bcebb560c2a2e2e2bb749e5fc728a
alison <adevpr11@gmail.com>
2026-06-13 13:28:37 -0300

    Atualiza ProdutoRepository

--- src/main/java/com/nca/productsapi/repositories/ProdutoRepository.java
+++ src/main/java/com/nca/productsapi/repositories/ProdutoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -14,20 +15,22 @@ public class ProdutoRepository {
 
     public void inserir(Produto obj) throws Exception {
 
-        try (var connection = ConnectionFactory.getConnection()) {
+        var sql =
+                """
+                        INSERT INTO produtos(nome, descricao, preco, quantidade)
+                          values(?,?,?,?)
+                        """;
 
-            var statement = connection.prepareStatement(
-                    """
-                            INSERT INTO produtos(nome, descricao, preco, quantidade)
-                              values(?,?,?,?)
-                            """);
+        try (var connection = ConnectionFactory.getConnection();
+             var preparedStatement = connection.prepareStatement(sql);
+        ) {
 
-            statement.setString(1, obj.getNome());
-            statement.setString(2, obj.getDescricao());
-            statement.setDouble(3, obj.getPreco());
-            statement.setDouble(4, obj.getQuantidade());
+            preparedStatement.setString(1, obj.getNome());
+            preparedStatement.setString(2, obj.getDescricao());
+            preparedStatement.setDouble(3, obj.getPreco());
+            preparedStatement.setDouble(4, obj.getQuantidade());
 
-            statement.execute();
+            preparedStatement.execute();
 
         } catch (SQLException e) {
             throw new RepositoryException("ERRO AO INSERIR DADOS: " + e.getMessage());
@@ -36,35 +39,43 @@ public class ProdutoRepository {
 
     public List<Produto> obterPorNome(String nome) throws Exception {
 
-        try (var connection = ConnectionFactory.getConnection()) {
+        if (nome == null || nome.isBlank()) {
+            return Collections.emptyList();
+        }
 
-            var statement = connection.prepareStatement(
-                    """
-                            SELECT id, nome, descricao, preco, quantidade
-                            from produtos
-                                        WHERE  ativo = 1 AND nome ilike ?
-                                        ORDER BY nome
-                            """);
+        nome = nome.trim();
+        
+        var sql =
+                """
+                        SELECT id, nome, descricao, preco, quantidade
+                        from produtos
+                                    WHERE  ativo = 1 AND nome ilike ?
+                                    ORDER BY nome
+                        """;
 
-            statement.setString(1, "%" + nome + "%");
+        try (var connection = ConnectionFactory.getConnection();
+             var preparedStatement = connection.prepareStatement(sql);
+        ) {
 
-            var result = statement.executeQuery();
+            preparedStatement.setString(1, "%" + nome + "%");
 
-            var lista = new ArrayList<Produto>();
+            try (var result = preparedStatement.executeQuery();) {
+                var lista = new ArrayList<Produto>();
 
-            while (result.next()) {
+                while (result.next()) {
 
-                var produto = new Produto();
+                    var produto = new Produto();
 
-                produto.setId(result.getInt("id"));
-                produto.setNome(result.getString("nome"));
-                produto.setDescricao(result.getString("descricao"));
-                produto.setPreco(result.getDouble("preco"));
-                produto.setQuantidade(result.getInt("quantidade"));
+                    produto.setId(result.getInt("id"));
+                    produto.setNome(result.getString("nome"));
+                    produto.setDescricao(result.getString("descricao"));
+                    produto.setPreco(result.getDouble("preco"));
+                    produto.setQuantidade(result.getInt("quantidade"));
 
-                lista.add(produto);
+                    lista.add(produto);
+                }
+                return lista;
             }
-            return lista;
         } catch (SQLException e) {
             throw new RepositoryException("ERRO AO OBTER DADOS: " + e.getMessage());
         }
@@ -72,15 +83,16 @@ public class ProdutoRepository {
 
     public boolean excluir(Integer id) throws Exception {
 
-        try (var connection = ConnectionFactory.getConnection()) {
-            var statement = connection.prepareStatement("""
-                        update produtos 
-                        set ativo = 0, data_exclusao = now()
-                        where id = ?
-                    """);
+        var sql = """
+                    update produtos
+                    set ativo = 0, data_exclusao = now()
+                    where id = ?
+                """;
 
-            statement.setInt(1, id);
-            var result = statement.executeUpdate();
+        try (var connection = ConnectionFactory.getConnection();
+             var preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setInt(1, id);
+            var result = preparedStatement.executeUpdate();
             return result > 0;
         } catch (SQLException e) {
             throw new RepositoryException("ERRO AO EXCLUIR DADOS: " + e.getMessage());
@@ -89,28 +101,32 @@ public class ProdutoRepository {
 
     public Produto obterPorId(Integer id) throws Exception {
 
-        try (var connection = ConnectionFactory.getConnection()) {
-            var statement = connection.prepareStatement("""
-                        select id, nome, descricao, preco, quantidade
-                        from produtos
-                        where ativo = 1 and id = ?
-                    """);
+        var sql = """
+                    select id, nome, descricao, preco, quantidade
+                    from produtos
+                    where ativo = 1 and id = ?
+                """;
 
-            statement.setInt(1, id);
-            var result = statement.executeQuery();
+        try (var connection = ConnectionFactory.getConnection();
+             var preparedStatement = connection.prepareStatement(sql);) {
 
-            Produto produto = null;
+            preparedStatement.setInt(1, id);
 
-            if (result.next()) {
-                produto = new Produto();
+            try (var result = preparedStatement.executeQuery();) {
+                Produto produto = null;
 
-                produto.setId(result.getInt("id"));
-                produto.setNome(result.getString("nome"));
-                produto.setDescricao(result.getString("descricao"));
-                produto.setPreco(result.getDouble("preco"));
-                produto.setQuantidade(result.getInt("quantidade"));
+                if (result.next()) {
+                    produto = new Produto();
+
+                    produto.setId(result.getInt("id"));
+                    produto.setNome(result.getString("nome"));
+                    produto.setDescricao(result.getString("descricao"));
+                    produto.setPreco(result.getDouble("preco"));
+                    produto.setQuantidade(result.getInt("quantidade"));
+                }
+                return produto;
+
             }
-            return produto;
         } catch (SQLException e) {
             throw new RepositoryException("ERRO AO OBTER POR ID: " + e.getMessage());
         }
@@ -118,19 +134,21 @@ public class ProdutoRepository {
 
     public boolean atualizar(Produto obj) throws Exception {
 
-        try (var connection = ConnectionFactory.getConnection()) {
-            var statement = connection.prepareStatement("""
-                        update produtos 
-                        set nome = ?, descricao = ?, preco = ?, quantidade = ?, data_atualizacao = now()
-                        where ativo = 1 and id = ?
-                    """);
+        var sql = """
+                    update produtos 
+                    set nome = ?, descricao = ?, preco = ?, quantidade = ?, data_atualizacao = now()
+                    where ativo = 1 and id = ?
+                """;
 
-            statement.setString(1, obj.getNome());
-            statement.setString(2, obj.getDescricao());
-            statement.setDouble(3, obj.getPreco());
-            statement.setInt(4, obj.getQuantidade());
-            statement.setInt(5, obj.getId());
-            var result = statement.executeUpdate();
+        try (var connection = ConnectionFactory.getConnection();
+             var preparedStatement = connection.prepareStatement(sql);) {
+
+            preparedStatement.setString(1, obj.getNome());
+            preparedStatement.setString(2, obj.getDescricao());
+            preparedStatement.setDouble(3, obj.getPreco());
+            preparedStatement.setInt(4, obj.getQuantidade());
+            preparedStatement.setInt(5, obj.getId());
+            var result = preparedStatement.executeUpdate();
 
             return result > 0;
         } catch (SQLException e) {
